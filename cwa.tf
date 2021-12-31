1f33bf0dd491576fdac69fa10bf137cb3daa4265
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-12-30 23:20:39 -0500

    cwa.tf

--- cwa.tf
+++ cwa.tf
@@ -16,5 +16,5 @@ resource "aws_cloudwatch_event_target" "this" {
   count               = "1"
   depends_on          = [aws_lambda_function.this]
   rule = aws_cloudwatch_event_rule.this.name
-  arn  = aws_lambda_function.this[count.index].invoke_arn
+  arn  = aws_lambda_function.this[count.index].function_name
 }
